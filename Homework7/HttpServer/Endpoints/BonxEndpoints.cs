59f6835b181dec935d88ec1d0d5aaf728de52499
pounceinwindow <adk03042006@gmail.com>
2025-11-08 08:51:17 +0300

    7

---
+++ Homework7/HttpServer/Endpoints/BonxEndpoints.cs
@@ -0,0 +1,13 @@
+﻿using HttpServer.Framework.core.Attributes;
+
+namespace HttpServer.Endpoints;
+
+[Endpoint]
+public class BonxEndpoing
+{
+    [HttpGet("/bonx")]
+    public string LoginPage()
+    {
+        return "bonx/about.html";
+    }
+}
