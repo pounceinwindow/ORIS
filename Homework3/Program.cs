f3b0bfbdb5813639afb791c31428bd3dcc698db0
pounceinwindow <adk03042006@gmail.com>
2025-12-13 09:35:50 +0300

    cleanup

---
+++ Homework3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using HttpServer.Shared;
+try
+{
+    if (!File.Exists("settings.json"))
+        throw new FileNotFoundException("Файл настроек не найден", "settings.json");
+    string settingsJson = File.ReadAllText("settings.json");
+    SettingsModel settings = JsonSerializer.Deserialize<SettingsModel>(settingsJson)
+                             ?? throw new JsonException("settings.json incorrect");
+    
+    var server = HttpServerApp.HttpServer.GetInstance(settings);
+    server.Start();
+    Console.WriteLine("нажмите /stop для завершения.\n");
+
+    while (true)
+    {
+        
+        var stop = Console.ReadLine();
+        if (stop?.Trim().ToLower() == "/stop")
+        {
+            server.Stop(); 
+            break; 
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Ошибка: " + e.Message);
+}
